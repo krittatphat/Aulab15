8ab1361f62cb8ed558d423e1044b0f03ac4e793b
Krittatphat <kittadpast.402@hotmail.com>
2023-01-17 17:02:25 +0700

    Completed

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,42 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void showMatrix(const bool A[][N]){
+    for(int i = 0 ; i < N ; i++){
+        for(int j = 0 ; j < N ; j++){
+            cout << A[i][j] << " " ;
+        }
+        cout << "\n" ;
+    }
+}
+
+void inputMatrix(double A[][N]){
+    for(int i = 0 ; i < N ; i++){
+        cout << "Row " << i+1 << ": " ;
+        for(int j = 0 ; j < N ; j++){
+        cin >> A[i][j] ;
+        }
+    }
+}
+
+void findLocalMax(const double A[][N], bool B[][N]){
+    for(int i = 1 ; i < N-1 ; i++){
+        for(int j = 1 ; j < N-1 ; j++){
+            if(A[i][j] >= A[i][j-1] && A[i][j] >= A[i][j+1] && A[i][j] >= A[i-1][j] && A[i][j] >= A[i+1][j]){
+                B[i][j] = 1 ;
+            }else{
+                B[i][j] = 0 ;
+            }
+        }
+    }
+    for(int x = 0 ; x < N ; x++){
+        B[x][0] = 0 ;
+        B[x][N-1] = 0 ;
+            for(int y = 0 ; y < N ; y++){
+                B[0][y] = 0 ;
+                B[N-1][y] = 0 ;
+            }
+    }
+}
+                
